11331edaa5506f2ac9390c5e4deb7f968bcfe5cb
aak-301 <76422079+aak-301@users.noreply.github.com>
2021-06-20 11:16:32 +0530

    Updated Tee implementation

--- C++ tree implementation.cpp
+++ C++ tree implementation.cpp
@@ -1,168 +1,221 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<queue>
 using namespace std;
 class BST{
-	int data;
-	BST *left;
-	BST *right;
-	public:
-		BST(){
-			data = 0;
-			left = right = NULL;
-		}
-		BST(int n){
-			data = n;
-			left = right = NULL;
-		}
-		BST* Insert(BST* head, int n);
-		void Inorder(BST* head);
-		void Preorder(BST* head);
-		void Postorder(BST* head);
-		int smallestElement(BST* head);
-		int largestElement(BST* head);
-		int totalNode(BST* head);
-		int height(BST* head);
-		int internalNodes(BST* head);
-		int totalLeafNodes(BST* head);
-		int search(BST* head, int n);
-		int InOrderSuccessor(BST* head);
-		BST* deleteElement(BST* head, int n);
-		void deleteNode(BST* head);
+    int val;
+    BST *left,*right;
+    queue<BST* > levelOrederQueue;
+    public:
+    BST();
+    BST(int val);
+    BST* Insert(BST* head, int val);
+    BST* Delete(BST* head, int val);
+    BST* mirrorImage(BST* head);
+    void Inorder(BST* head);
+    void Preorder(BST* head);
+    void Postorder(BST* head);
+    void leafNode(BST* head);
+    void Levelorder(BST* head);
+    void InternalNode(BST* head);
+    int LargestNode(BST* head);
+    int SmallestNode(BST* head);
+    int Height(BST* node);
+    bool hasElement(BST* head, int val);
 };
-BST* BST::Insert(BST* head, int n){
-	if(head==NULL) return new BST(n);
-	else if(head->data < n) head->right = Insert(head->right,n);
-	else head->left = Insert(head->left,n);
+BST::BST(){
+    val=0;
+    left=right=NULL;
 }
-
+BST::BST(int data){
+    val=data;
+    left=right=NULL;    
+}
 void BST::Inorder(BST* head){
-	if(head == NULL) return;
-	Inorder(head->left);
-	cout<<head->data<<" ";
-	Inorder(head->right);
+    if( !head ) return;
+    Inorder(head->left);
+    cout<<head->val<<" ";
+    Inorder(head->right);
 }
-
 void BST::Preorder(BST* head){
-	if(head == NULL) return;
-	else cout<<head->data<<" ";
-	Preorder(head->left);
-	Preorder(head->right);
+    if(!head) return;
+    cout<<head->val<<" ";
+    Preorder(head->left);
+    Preorder(head->right);
 }
-
 void BST::Postorder(BST* head){
-	if(head == NULL) return;
-	Postorder(head->left);
-	Postorder(head->right);
-	cout<<head->data<<" ";
+    if(!head) return;
+    Postorder(head->left);
+    Postorder(head->right);
+    cout<<head->val<<" ";
 }
+void BST::Levelorder(BST* head){
+    if(!head) return;
+    levelOrederQueue.push(head);
+    while(!levelOrederQueue.empty()){
+        BST* temp = levelOrederQueue.front();
+        cout<<temp->val<<" ";
+        levelOrederQueue.pop();
 
-int BST::smallestElement(BST* head){
-	if(head->left == NULL) return head->data;
-	else return smallestElement(head->left);
+        if(temp->left != NULL)
+            levelOrederQueue.push(temp->left);
+        if(temp->right != NULL)
+            levelOrederQueue.push(temp->right);
+    }
 }
-
-int BST::largestElement(BST* head){
-	if(head->right == NULL) return head->data;
-	else return largestElement(head->right);
+void BST::leafNode(BST* head){
+    if(! head) return;
+    if(head->left==NULL && head->right==NULL){
+        cout<<head->val<<" ";
+        return;
+    }
+    leafNode(head->left);
+    leafNode(head->right);
 }
-
-int BST::totalNode(BST* head){
-	if(head == NULL) return 0;
-	else 
-	return (totalNode(head->left) + totalNode(head->right) +1);
+int BST:: LargestNode(BST* head){
+    if(head->right==NULL) return head->val;
+    LargestNode(head->right);
 }
-
-int BST::height(BST* head){
-	if (head == NULL) return 0;
-	int left_height = height(head->left)+1;
-	int right_height = height(head->right)+1;
-	if(left_height > right_height) return left_height;
-	else return right_height;
+int BST:: SmallestNode(BST* head){
+    if(head->left==NULL) return head->val;
+    SmallestNode(head->left);
 }
-
-int BST::internalNodes(BST* head){
-	if(head == NULL || (head->left == NULL && head->right == NULL)) return 0;
-	else return (internalNodes(head->left) + internalNodes(head->right) + 1);
+int BST::Height(BST* head){
+    if(!head) return 0;
+    if(head->left == NULL && head->right == NULL){
+        return 1;
+    }
+    if(Height(head->left)+1 > Height(head->right)+1)
+    return Height(head->left)+1 ;
+    else return Height(head->right)+1;
 }
-
-int BST::totalLeafNodes(BST* head){
-	if(head == NULL || (head->left == NULL && head->right == NULL)) return 1;
-	else return (internalNodes(head->left) + internalNodes(head->right));
+bool BST::hasElement(BST* head, int data){
+    if(!head) return false;
+    else if(head->val == data) return true;
+    if(hasElement(head->left,data)||hasElement(head->right,data)) return true;
 }
-
-int BST::search(BST* head, int n){
-	if(head == NULL) return 0;
-	else if(head->data == n) return 1;
-	return search(head->left,n) + search(head->right,n);
+void BST:: InternalNode(BST* head){
+    if(!head) return ;
+    if(head->left==NULL && head->right==NULL){
+        return;
+    }
+    cout<<head->val<<" ";
+    InternalNode(head->left);
+    InternalNode(head->right);
 }
-
-int BST::InOrderSuccessor(BST* head){
-	int min;
-	while(head->left != NULL){
-		min = head->left->data;
-		head = head->left;
-	}
-	return min;
+BST* BST::mirrorImage(BST* head){
+    BST* temp;
+    while(!head){
+        mirrorImage(head->left);
+        mirrorImage(head->right);
+        temp = head->right;
+        head->right = head->left;
+        head->left = temp;
+    }
 }
-
-BST* BST::deleteElement(BST* head, int n){
-	if(head == NULL) return head;
-	else if(n < head->data)	
-	head->left =  deleteElement(head->left,n);
-	else if(n > head->data) 
-	head->right = deleteElement(head->right,n);
-	else{
-		if(head->left == NULL) {
-			return head->right;
-		}
-		else if(head->right == NULL){
-			return head->left;			
-		}
-		head->data = InOrderSuccessor(head->right);
-		head->right = deleteElement(head->right, head->data);	
-	}
-	return head;
+BST* BST::Insert(BST* head, int data){
+    if(!head)
+         return new BST(data);
+    if(data>head->val)
+        head->right = Insert(head->right, data);
+    else
+        head->left = Insert(head->left, data);
+    return head;
 }
+BST* BST::Delete(BST* head, int data){
+    if(!head) return NULL;
+    else if(data < head->val){
+        head->left = Delete(head->left,data);
+    }
+    else if(data > head->val){
+        head->right = Delete(head->right,data);
+    }
+    else{
+        if(head->left==NULL && head->right == NULL){
+            return NULL;
+        }
+        else if(head->left == NULL){
+            BST* temp = head->right;
+            free (head);
+            return temp;
+        }
+        else if(head->right == NULL){
+            BST* temp = head->left;
+            free (head);
+            return temp;
+        }
+        int small = SmallestNode(head->right);
+        head->val = small;
 
-void BST::deleteNode(BST* head){
-	if (head != NULL){
-		deleteNode(head->left);
-		deleteNode(head->right);
-		delete head;		
-	}	
+        head->right = Delete(head->right, small);
+
+        return head;        
+    }
 }
-
 int main(){
-	int r,n,v,d;
-	cin>>n;
-	BST b, *root = NULL;
-	cin>>r;
-	root = b.Insert(root,r);	
-	for(int i=1;i<n;i++){
-		cin>>v;
-		b.Insert(root,v);
-	}
-	b.Preorder(root);
-	cout<<endl;
-	b.Inorder(root);
-	cout<<endl;
-	b.Postorder(root);
-	cout<<endl<<"Samllest element: "<<b.smallestElement(root);
-	cout<<endl<<"Largest element: "<<b.largestElement(root);
-	cout<<endl<<"Total number of nodes: "<<b.totalNode(root);
-	cout<<endl<<"Height of tree: "<<b.height(root);
-	cout<<endl<<"number of internal nodes: "<<b.internalNodes(root);
-	cout<<endl<<"number of leaf nodes: "<<b.totalLeafNodes(root);
-	cout<<endl<<"Enter the elemnet to be searched: ";
-	cin>>d;
-	if(b.search(root, d)) cout<<endl<<"Element found";
-	else cout<<endl<<"Element not found";
-	b.deleteNode(root);
-	cout<<endl<<"Enter the elemnet to be deleted: ";
-	cin>>d;
-	if(b.search(root, d)) {
-		b.deleteElement(root, d);
-		cout<<endl<<"Element found and deleted";
-	}	
-	else cout<<endl<<"Element not found"<<endl;
-	b.Preorder(root);
+
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    BST tree;
+    BST *head = NULL;
+    int t;
+    cin>>t;
+    int delete_element, occured;
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        if(i==0)
+            head = tree.Insert(head,n);
+        else
+            tree.Insert(head,n);
+    }
+    cout<<"Preorder Traversal: ";
+    tree.Preorder(head);
+    cout<<endl;
+    cout<<"Inorder Traversal: ";
+    tree.Inorder(head);
+    cout<<endl;
+    cout<<"Postorder Traversal: ";
+    tree.Postorder(head);
+    cout<<endl;
+    cout<<"Levelorder Traversal: ";
+    tree.Levelorder(head);
+    cout<<endl;
+    cout<<"leafNodes are: ";
+    tree.leafNode(head);
+    cout<<endl;
+    cout<<"InternalNodes are: ";
+    tree.InternalNode(head);
+    cout<<endl;
+    cout<<"LargestNode: ";
+    cout<<tree.LargestNode(head)<<endl;
+    cout<<"SmallestNode: ";
+    cout<<tree.SmallestNode(head)<<endl;
+    cout<<"Height: ";
+    cout<<tree.Height(head)<<endl;
+    cout<<"Enter the element to search: ";
+    cin>>occured;
+    cout<<occured<<endl;
+    if(tree.hasElement(head, occured)) cout<<"Yes "<<occured<<" is present\n";
+    else  cout<<"No "<<occured<<" is not present\n";
+    cout<<"Enter a number to delete: ";
+    cin>>delete_element;
+    cout<<delete_element<<endl;
+    if(tree.hasElement(head, delete_element)){
+        tree.Delete(head, delete_element); cout<<"Deleted "<<delete_element<<endl;
+    }
+    else  cout<<"There isn't any "<<delete_element<<endl;
+    cout<<endl;
+    tree.mirrorImage(head);
+    cout<<"After finding mirror Image of tree: \n";
+    cout<<"Preorder Traversal: ";
+    tree.Preorder(head);
+    cout<<endl;
+    
 }
